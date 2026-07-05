42853a256d24195d276c257673704e2267333f3f
omersabri <69110814+omersabri@users.noreply.github.com>
2026-07-05 19:13:37 +0000

    Refactor program into main function and improve code structure

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from helper import calculate_total, generate_summary
 
-price = 5
-quantity = 3
 
-total = calculate_total(price, quantity)
+def main():
+    price = 5
+    quantity = 3
 
-print(f"The total is £{total}")
-print(generate_summary(total))
+    total = calculate_total(price, quantity)
+
+    print(f"The total is £{total}")
+    print(generate_summary(total))
+
+
+if __name__ == "__main__":
+    main()
