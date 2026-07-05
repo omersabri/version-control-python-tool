ed2c1577a8277cbb1ba5fc18fdcb3563f9306ee1
omersabri <69110814+omersabri@users.noreply.github.com>
2026-07-05 18:28:48 +0000

    Add initial Python calculator files

---
+++ helper.py
@@ -0,0 +1,6 @@
+def calculate_total(price, quantity):
+    return price * quantity
+
+
+def generate_summary(total):
+    return f"Summary: the final total is £{total}"
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_total(price, quantity):
-    return price * quantity
+from helper import calculate_total, generate_summary
 
 price = 5
 quantity = 3
+
 total = calculate_total(price, quantity)
+
 print(f"The total is £{total}")
+print(generate_summary(total))
